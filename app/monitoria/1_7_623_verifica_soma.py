08f21589806aff8025d268c7cbf32f647b6ee430
João Pedro Pinheiro <joaopedro.pinheiro88@gmail.com>
2019-04-03 19:23:26 -0300

    Busca Binária retorna a posição, não o elemento
    Alterando o script para considerar o elemento de v2.

--- app/monitoria/1_7_623_verifica_soma.py
+++ app/monitoria/1_7_623_verifica_soma.py
@@ -8,9 +8,9 @@ def verifica_soma(s1, s2, x):
     v2 = merge_sort(s2)
     for e1 in v1:
         e = x - e1
-        e2 = binary_search(v2, e)
-        if e2 is not None:
-            return e1, e2
+        p2 = binary_search(v2, e)
+        if p2 is not None:
+            return e1, v2[p2]
     return None
 
 
@@ -19,5 +19,5 @@ if __name__ == '__main__':
     print(l1)
     l2 = generate_list(25)
     print(l2)
-    s = 10
+    s = 40
     print(verifica_soma(l1, l2, s))
