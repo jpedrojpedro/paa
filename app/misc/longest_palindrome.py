4fc532c9fde54b0b1f3bdc14b64e379b5dce6b55
João Pedro Pinheiro <joaopedro.pinheiro88@gmail.com>
2020-11-28 22:25:35 -0300

    Longest palindrome working but taking too long

--- app/misc/longest_palindrome.py
+++ app/misc/longest_palindrome.py
@@ -2,17 +2,36 @@ from utils.measures import log_time
 
 
 def longest_palindrome(s):
-    if len(s) == 1:
+    ws = len(s)
+    if ws <= 1:
         return s
-    ls = [c for c in s]
-    ls.reverse()
-    rls = ''.join(ls)
-    if s == rls:
-        return s
-    longest_palindrome(s[:-1])
+    while ws > 0:
+        for i in range(len(s)):
+            j = i + ws
+            if j > len(s):
+                break
+            ss = s[i:j]
+            rs = [c for c in ss]
+            rs.reverse()
+            rs = ''.join(rs)
+            if ss == rs:
+                return ss
+        ws -= 1
+    return ''
 
 
 if __name__ == '__main__':
-    aux = 'abbaunfhannah'
-    t, res = log_time(longest_palindrome, aux)
+    bar = "abbacdhannah"
+    t, res = log_time(longest_palindrome, bar)
+    print("Solution: {} | {} s".format(res, t))
+    foo = "ibvjkmpyzsifuxcabqqpahjdeuzaybqsrsmbfplxycsafogotliyvhxjtkrbzqxlyfwujzhkdafhebvsdhkkdbhlhmaoxmbkqiwiusngkb" \
+          "dhlvxdyvnjrzvxmukvdfobzlmvnbnilnsyrgoygfdzjlymhprcpxsnxpcafctikxxybcusgjwmfklkffehbvlhvxfiddznwumxosomfbgx" \
+          "oruoqrhezgsgidgcfzbtdftjxeahriirqgxbhicoxavquhbkaomrroghdnfkknyigsluqebaqrtcwgmlnvmxoagisdmsokeznjsnwpxygj" \
+          "jptvyjjkbmkxvlivinmpnpxgmmorkasebngirckqcawgevljplkkgextudqaodwqmfljljhrujoerycoojwwgtklypicgkyaboqjfivbeq" \
+          "dlonxeidgxsyzugkntoevwfuxovazcyayvwbcqswzhytlmtmrtwpikgacnpkbwgfmpavzyjoxughwhvlsxsgttbcyrlkaarngeoaldsdtj" \
+          "ncivhcfsaohmdhgbwkuemcembmlwbwquxfaiukoqvzmgoeppieztdacvwngbkcxknbytvztodbfnjhbtwpjlzuajnlzfmmujhcggpdcwdq" \
+          "uutdiubgcvnxvgspmfumeqrofewynizvynavjzkbpkuxxvkjujectdyfwygnfsukvzflcuxxzvxzravzznpxttduajhbsyiywpqunnarab" \
+          "croljwcbdydagachbobkcvudkoddldaucwruobfylfhyvjuynjrosxczgjwudpxaqwnboxgxybnngxxhibesiaxkicinikzzmonftqkcud" \
+          "lzfzutplbycejmkpxcygsafzkgudy"
+    t, res = log_time(longest_palindrome, foo)
     print("Solution: {} | {} s".format(res, t))
