961d92b8d4ce5ca22c998084187553c04bcef7bf
João Pedro Pinheiro <joaopedro.pinheiro88@gmail.com>
2020-11-28 23:23:48 -0300

    Longest palidrome with sliding window and recursive check

--- app/misc/longest_palindrome.py
+++ app/misc/longest_palindrome.py
@@ -1,28 +1,31 @@
 from utils.measures import log_time
 
 
+def is_palindrome(s):
+    if len(s) == 0 or len(s) == 1:
+        return True
+    if s[0] == s[-1]:
+        return is_palindrome(s[1:-1])
+    return False
+
+
 def longest_palindrome(s):
     ws = len(s)
-    if ws <= 1:
-        return s
     while ws > 0:
         for i in range(len(s)):
             j = i + ws
             if j > len(s):
                 break
             ss = s[i:j]
-            rs = [c for c in ss]
-            rs.reverse()
-            rs = ''.join(rs)
-            if ss == rs:
+            if is_palindrome(ss):
                 return ss
         ws -= 1
     return ''
 
 
 if __name__ == '__main__':
-    bar = "abbacdhannah"
-    t, res = log_time(longest_palindrome, bar)
+    foo = "abbacdhannah"
+    t, res = log_time(longest_palindrome, foo)
     print("Solution: {} | {} s".format(res, t))
     foo = "ibvjkmpyzsifuxcabqqpahjdeuzaybqsrsmbfplxycsafogotliyvhxjtkrbzqxlyfwujzhkdafhebvsdhkkdbhlhmaoxmbkqiwiusngkb" \
           "dhlvxdyvnjrzvxmukvdfobzlmvnbnilnsyrgoygfdzjlymhprcpxsnxpcafctikxxybcusgjwmfklkffehbvlhvxfiddznwumxosomfbgx" \
@@ -33,5 +36,17 @@ if __name__ == '__main__':
           "uutdiubgcvnxvgspmfumeqrofewynizvynavjzkbpkuxxvkjujectdyfwygnfsukvzflcuxxzvxzravzznpxttduajhbsyiywpqunnarab" \
           "croljwcbdydagachbobkcvudkoddldaucwruobfylfhyvjuynjrosxczgjwudpxaqwnboxgxybnngxxhibesiaxkicinikzzmonftqkcud" \
           "lzfzutplbycejmkpxcygsafzkgudy"
+    t, res = log_time(longest_palindrome, foo)
+    print("Solution: {} | {} s".format(res, t))
+    foo = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" \
+          "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" \
+          "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" \
+          "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" \
+          "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabcaaaaaaaaaaaaaaaaaaaaaaaaaaaa" \
+          "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" \
+          "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" \
+          "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" \
+          "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa" \
+          "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
     t, res = log_time(longest_palindrome, foo)
     print("Solution: {} | {} s".format(res, t))
